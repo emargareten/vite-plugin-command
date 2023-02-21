2f5e2d207c7887747690c1b4aec0619bbea5d09e
emargareten <2771355@gmail.com>
2023-02-21 12:38:24 +0200

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:node/recommended",
     "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser"
